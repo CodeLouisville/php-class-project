7c513ee1cd4c30121f3a05c18a837bd6bddcd794
Jason McCreary <jason@pureconcepts.net>
2017-01-07 10:25:15 -0500

    Use arrays, loops, and conditionals

--- src/todos.php
+++ src/todos.php
@@ -1,7 +1,21 @@
 <?php
-// output a sample todo list
-echo "Todos:\n";
+// use array to hold todo items
+$todo_items = [
+    'Task 1',
+    'Task 2',
+    'Task 3'
+];
 
-echo "- Task 1\n";
-echo '- Task 2' . "\n";
-echo '- Task 3' . PHP_EOL;
+// output todo list from array
+echo 'Todos:' . PHP_EOL;
+
+$length = count($todo_items);
+for ($i = 0; $i < $length; ++$i) {
+    echo '  - ' . $todo_items[$i];
+
+    if ($i === 1) {
+        echo ' (completed)';
+    }
+
+    echo PHP_EOL;
+}
